859c9f98e496f4c3ba0da397743577336c2eba80
Inna Dmytrenko <inna.shalun@gmail.com>
2024-11-15 13:13:26 +0100

    finished HW10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,23 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+    <div className={s.loader}>
+        <svg viewBox="0 0 108 108" xmlns="http://www.w3.org/2000/svg">
+            <defs>
+                <linearGradient id="spinnerGradient" gradientTransform="rotate(90)">
+                    <stop offset="0%" stopColor="rgb(147, 51, 234)" />
+                    <stop offset="100%" stopColor="rgb(147, 51, 234)" stopOpacity="0.1" />
+                </linearGradient>
+            </defs>
+            <circle
+                className={s.circle}
+                cx="54"
+                cy="54"
+                r="45"
+                fill="none"
+                strokeWidth="12"
+                stroke="url(#spinnerGradient)"
+            />
+        </svg>
+    </div>
+)
